c6950973802c7a78976edaaee2ff99557e796df8
kj7kunal <kjkunal7996@gmail.com>
2021-10-29 20:26:16 +0530

    fixed scripts

--- scripts/gtg_gif.py
+++ scripts/gtg_gif.py
@@ -1,10 +1,18 @@
-from PIL import Image
 import glob
 
+from PIL import Image
+
+from text_draw import generate_answer_image
+
+
 GIF_PATH = "./gifs/"
+SAVE_PATH = "./generated/{}.png"
+ANSWER_PATH = "./answers/{}.png"
+
 TOTALFRAMES = 18
 FRAME_SIZE = (200, 128)
-SAVE_PATH = "./generated/{}.png"
+
+# GIFs -> Sprite Sheets
 
 gif_files = sorted(glob.glob(GIF_PATH + '*.gif'))
 
@@ -17,41 +25,74 @@ for fn in gif_files:
     gif = Image.open(fn)
     # number of frames for gif
     n_frames = gif.n_frames
-    print(f"{n_frames} frames in {fn}")
     
+    # list of selected frames
     selected_frames = frame_selector(n_frames, TOTALFRAMES)
 
     # initialize empty PIL Images
-    gif_sprite = Image.new("RGB", (FRAME_SIZE[0], TOTALFRAMES * FRAME_SIZE[1]), color=(255,255,255))
-    frame_image = Image.new("RGB", gif.size, color=(255,255,255))
+    gif_sprite = Image.new(
+        "RGB", 
+        (FRAME_SIZE[0], TOTALFRAMES * FRAME_SIZE[1]), 
+        color=(255,255,255)
+    )
+    frame_image = Image.new(
+        "RGB", 
+        gif.size, 
+        color=(255,255,255)
+    )
     
     ycoord = 0
     for frame in selected_frames:
         gif.seek(frame)
         frame_image.paste(gif)
-        gif_sprite.paste(frame_image.resize(FRAME_SIZE, Image.ANTIALIAS).convert('RGB'), (0, ycoord))
+        gif_sprite.paste(
+            frame_image.resize(FRAME_SIZE, Image.ANTIALIAS).convert('RGB'), 
+            (0, ycoord)
+        )
         ycoord += FRAME_SIZE[1]
         
     if (ycoord // FRAME_SIZE[1]) == TOTALFRAMES:
         gif_sprite.save(SAVE_PATH.format(fn.split('/')[-1][:-4]))
-        print((ycoord//FRAME_SIZE[1]), " = ", (TOTALFRAMES))
-    else:
-        print((ycoord//FRAME_SIZE[1]), " != ", (TOTALFRAMES))
         
-# Sprite Grid
+# Sprite Sheets -> Sprite Grids
+
 sprites = sorted(glob.glob(SAVE_PATH.format('*')))
-choice = [1, 5, 6, 8, 18, 20, 27, 28, 30, 37, 54]
 
-merged_sprite = Image.new("RGB", (FRAME_SIZE[0]*len(choice), TOTALFRAMES * FRAME_SIZE[1]), color=(255,255,255))
+N_MERGE = 11
 
-xcoord = 0
+# Repeat sprites to make multiple of 11
+sprites += sprites[: (N_MERGE - len(sprites) % N_MERGE)]
+assert len(sprites)//N_MERGE == 0, "Number of sprites should be a multiple of 11"
 
-for x in choice:
-    print(x)
-    im = Image.open(SAVE_PATH.format("g%03d"%(x)))
-    merged_sprite.paste(im, (xcoord, 0))
-    xcoord += FRAME_SIZE[0]
+with open("./gifs/gif_names.txt", "r") as f:
+    gif_names = [x.strip() for x in f.readlines()]
+len(gif_names), gif_names[0]
+
+ct = 0
+for i in range(len(sprites) // N_MERGE):
+    print(i, ct)
+    merged_sprite = Image.new(
+        "RGB", 
+        (FRAME_SIZE[0] * N_MERGE, TOTALFRAMES * FRAME_SIZE[1]), 
+        color=(255,255,255)
+    )
+    xcoord = 0
     
-merged_sprite.save(f"./chosen_{len(choice)}.png")
-
-merged_sprite.resize(((merged_sprite.size[0] * 1024 // merged_sprite.size[1]), 1024), Image.ANTIALIAS).save(f"./chosen_{len(choice)}_1024.png")
+    for x in sprites[i*N_MERGE: (i+1)*N_MERGE]:
+        im = Image.open(x)
+        merged_sprite.paste(im, (xcoord, 0))
+        xcoord += FRAME_SIZE[0]
+        
+        # save answer image
+        generate_answer_image(gif_names[int(x.split('/')[-1][1:-4]) - 1], ct, ANSWER_PATH)
+        
+        ct += 1
+    
+    # save sprite grid
+    merged_sprite.save(f"./chosen_{i}.png")
+    
+    # save sprite grid resized to maximum of (1024 x 1024) as per SparkAR guidelines
+    merged_sprite.resize(
+        (merged_sprite.size[0]*1024//merged_sprite.size[1], 1024), 
+        Image.ANTIALIAS
+    ).save(f"./chosen_{i}_1024.png")
